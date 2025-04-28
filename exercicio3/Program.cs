48a31a84b6d9547bc209cdd67aa789e133393473
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2025-04-27 21:34:19 -0300

    Add exercise

--- exercicio3/Program.cs
+++ exercicio3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var rectangleBase = InputTypeDouble("Digite a base do retângulo: (em metros) ");
+        var rectangleHeight = InputTypeDouble("Digite a altura do retângulo: (em metros) ");
+
+        Func<double, double, double> calculateArea = CalculateRectangleArea;
+
+        var area = calculateArea(rectangleBase, rectangleHeight);
+
+        Console.WriteLine($"A área do retângulo é: {area}");
+    }
+
+    private static double CalculateRectangleArea(double baseValue, double heightValue)
+    {
+        return baseValue * heightValue;
+    }
+
+    private static double InputTypeDouble(string message)
+    {
+        Console.Write(message);
+        if (double.TryParse(Console.ReadLine() ?? "0", out var value))
+        {
+            return value;
+        }
+
+        Console.WriteLine("Valor inválido, tente novamente.");
+        return InputTypeDouble(message);
     }
 }
