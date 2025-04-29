2782bef8eb7223269b07f3626f63c75f68f61178
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2025-04-28 22:19:40 -0300

    Add exercise

---
+++ exercicio8/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace exercicio8.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public List<Product> Products { get; set; } = [];
+
+    public void OnGet()
+    {
+        Products =
+        [
+            new Product { Name = "Banana", Price = 1.20m },
+            new Product { Name = "Maçã", Price = 2.50m },
+            new Product { Name = "Laranja", Price = 1.80m }
+        ];
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ exercicio8/Pages/More.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace exercicio8.Pages;
+
+public class MoreModel : PageModel
+{
+    private readonly ILogger<MoreModel> _logger;
+
+    public MoreModel(ILogger<MoreModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
