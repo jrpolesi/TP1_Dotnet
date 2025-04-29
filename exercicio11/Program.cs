38100d8ea854f1583df8a71d4e64024a44f3ab76
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2025-04-29 02:09:23 -0300

    Add exercise

---
+++ exercicio11/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace exercicio11;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Func<string, string, string>? processName = ConcatFullName;
+        processName += (string firstName, string lastName) => ToUpper(ConcatFullName(firstName, lastName));
+        processName += (string firstName, string lastName) => RemoveSpaces(ConcatFullName(firstName, lastName));
+
+        var name = "João";
+        var lastName = "Silva";
+
+        var result = processName?.Invoke(name, lastName);
+
+        // Após analisar o retorno é possível verificar que por mais que todos os métodos 
+        // foram executado, porém o resultado corresponde apenas ao último método.
+        Console.WriteLine(result); // JoãoSilva
+    }
+
+    private static string ConcatFullName(string firstName, string lastName)
+    {
+        return $"{firstName} {lastName}";
+    }
+
+    private static string ToUpper(string str)
+    {
+        return str.ToUpper();
+    }
+
+    private static string RemoveSpaces(string str)
+    {
+        return str.Replace(" ", "");
+    }
+}
